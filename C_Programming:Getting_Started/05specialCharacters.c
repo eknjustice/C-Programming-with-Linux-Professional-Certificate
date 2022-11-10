f6f745fe220b27fd0892edf1e8f6e810cbbc4e8a
eknjustice <eknjustice@gmail.com>
2022-11-10 21:38:45 +0200

    escape sequences

---
+++ C_Programming:Getting_Started/05specialCharacters.c
@@ -0,0 +1,17 @@
+/*
+ Your goal is to print Dennis Ritchie's famous quote to the screen just as shown below. 
+ To make this task a bit harder, you are only allowed to use one printf statement in your entire program. 
+ Be sure not to omit anything and not to print anything extra.
+ 
+ Output:
+ Dennis Ritchie said:                                                            
+ "The only way to learn a new programming language is by writing programs in it."
+*/
+
+//use of \n and \"
+#include <stdio.h>
+int main()
+{
+	printf("Dennis Ritchie said:\n\"The only way to learn a new programming language is by writing programs in it.\"");
+}
+
