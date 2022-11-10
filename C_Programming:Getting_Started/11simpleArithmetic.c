ebe9bddc0d13a5586c3be6fb777fb8166ef3443d
eknjustice <eknjustice@gmail.com>
2022-11-10 19:36:34 +0200

    perform simple arithmetic in C

---
+++ C_Programming:Getting_Started/11simpleArithmetic.c
@@ -0,0 +1,23 @@
+/*
+You work for the IBP (International Bureau of Procrastination). 
+You've been asked how much time is left until the official day of procrastination (March 25th).
+
+Given that you've been asked on March 23rd, 
+please write a C-program which performs arithmetic in order to produce the following output:
+
+Dear Procrastinator,
+You still have to wait for X days (Y minutes or Z seconds) before you can procrastinate!
+Here, X is the remaining number of days (25-23), Y is the number of minutes (60 * 24 * X) 
+and Z is the number of seconds (60 * Y). 
+The sentence has to be exactly the one displayed above, replacing X, Y and Z with the computed values. 
+The format has to be followed precisely.
+
+You cannot simply perform these calculations yourself and print the values - 
+your program must calculate them and print them using the %d format specifier.
+*/
+
+#include <stdio.h>
+int main()
+{
+    printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!\n", 25-23, (25-23)*24*60, (25-23)*24*60*60);
+}
