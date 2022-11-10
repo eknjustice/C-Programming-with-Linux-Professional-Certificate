05fec3e71b47ac844ed33b13722b28f2edf04847
eknjustice <eknjustice@gmail.com>
2022-11-10 20:33:33 +0200

    print multiple lines with one printf statement

---
+++ C_Programming:Getting_Started/07multipleLines.c
@@ -0,0 +1,14 @@
+/*Your goal is to display the piece of abstract art shown below, using just a single printf command.
+
+*****
+**|**
+*|.|*
+|...|
+.....
+*/
+
+#include <stdio.h>
+int main()
+{
+	printf("*****\n**|**\n*|.|*\n|...|\n.....\n");
+}
