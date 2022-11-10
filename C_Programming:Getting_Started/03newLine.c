2a06aeacd96c203668fb4d4a40812e8b171a8184
eknjustice <eknjustice@gmail.com>
2022-11-10 21:17:36 +0200

    print text over several line

---
+++ C_Programming:Getting_Started/03newLine.c
@@ -0,0 +1,19 @@
+/*
+Print a list of skills you have already mastered to the screen.
+The list of skills is the following:
+I already know how to:
+- Print text to the screen.
+- Start a new line.
+- Fix errors.
+*/
+
+//used \n to create a new line after every string
+
+#include <stdio.h>
+int main()
+{
+	printf("I already know how to:\n");
+	printf("- Print text to the screen.\n");
+	printf("- Start a new line.\n");
+	printf("- Fix errors.\n");
+}
