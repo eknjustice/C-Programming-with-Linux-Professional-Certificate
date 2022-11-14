05aa8e531c4610724bc05dd9bfb313621c615386
eknjustice <eknjustice@gmail.com>
2022-11-14 15:37:24 +0200

    read characters with scanf

---
+++ C_Programming:Getting_Started/16readCharacters.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char letter1;
+    char letter2 = '+';
+    scanf("%c", &letter1);
+    printf("%c%c%c%c%c%c%c%c%c\n",letter2,letter2,letter2,letter2,letter1,letter2,letter2,letter2,letter2);
+    printf("%c%c%c%c%c%c%c%c%c\n",letter2,letter2,letter2,letter1,letter1,letter1,letter2,letter2,letter2);
+    printf("%c%c%c%c%c%c%c%c%c\n",letter2,letter2,letter1,letter1,letter1,letter1,letter1,letter2,letter2);
+    printf("%c%c%c%c%c%c%c%c%c\n",letter2,letter1,letter1,letter1,letter1,letter1,letter1,letter1,letter2);
+    printf("%c%c%c%c%c%c%c%c%c\n",letter1,letter1,letter1,letter1,letter1,letter1,letter1,letter1,letter1);
+}
